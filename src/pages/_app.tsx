4cd4da378f891f6629d85c726bdc70a40f33a279
Samuel Fagundes <samuelfagundessva@gmail.com>
2022-10-31 20:02:24 -0300

    header styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,20 @@
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { AppProps } from "next/app";
+import { globalStyles } from "../styles/global";
+import { Container, Header } from "../styles/pages/app";
+
+import logoImg from '../assets/logo.svg'
+import Image from "next/image";
+
+export default function App({ Component, pageProps }: AppProps) {
+
+  globalStyles()
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <h1>hello world</h1>
+    <h1>.</h1>
   )
 }
